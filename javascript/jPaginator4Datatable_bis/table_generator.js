60da16c79c421bb100a4e9d28f9fa56a65dafb67
Luca Lazzarini <lzzluca@gmail.com>
2014-02-20 18:15:52 +0000

    Improved the table generator to provide a better demo

--- javascript/jPaginator4Datatable_bis/table_generator.js
+++ javascript/jPaginator4Datatable_bis/table_generator.js
@@ -1,83 +1,92 @@
-var TableGenerator = function() {};
-TableGenerator.prototype = {
+var TableGenerator = function(settings) {
+  if( typeof settings !== "object" || settings === null ) {
+    settings = this.defaults;
+  }
+  
+  for(var i in settings) {
+    settings[i] = this.defaults[i];
+  }
+  
+  this.settings = settings;  
+};
 
-    options: {
-        nRowsH: 1,              // thead rows number
-        nRowsB: 100,            // tbody rows number
-        nRowsF: 0,              // tfoot rows number
-        nCols: 10,              // columns number
-        thContent: "demo head", // th default text
-        tdContent: "demo",      // td default text
-        fillParent: false,      // the table will fill the parent
-        hasRowTitle: false      // adds a th as first cell of every rows
-    },
+TableGenerator.prototype.defaults = {
+        nRowsH: 1,                                        // thead rows number
+        nRowsB: 100,                                      // tbody rows number
+        nRowsF: 0,                                        // tfoot rows number
+        nCols: 10,                                        // columns number
+        hasRowTitle: false,                               // adds a th as first cell of every rows TODO should be a number like above
+        thContent: function(x) { return x; },             // th default text
+        tdContent: function(x, y) { return x + " " +y; }, // td default text
+        thr: function(y) { return y; },                   // row title default text
+        fillParent: false                                 // the table will fill the size of the parent
 
-    getHTML: function () {
+};
+
+TableGenerator.prototype.getHTML = function () {
         return this._generateHTML();
-    },
+};
 
-    _generateHTML: function () {
+TableGenerator.prototype._generateHTML = function () {
         var optStyle = "";
-        if (this.options.fillParent)
+        if (this.settings.fillParent)
             optStyle += "table-layout: fixed; width: 100%; height: 100%;";
         return "<TABLE class = 'table' style = '" + optStyle + "'>" + 
                     this._generateHTMLHead() + 
                     this._generateHTMLBody() +
                     this._generateHTMLFoot() + 
                 "</TABLE>";
-    },
+};
 
-    _generateHTMLHead: function() {
+TableGenerator.prototype._generateHTMLHead = function() {
         var HTML = "<THEAD class = 'thead'>";
 
-        for (var i = 0; i < this.options.nRowsH; i++) {
+        for (var i = 0; i < this.settings.nRowsH; i++) {
 
             HTML += "<TR class = 'trh'>"
-            if(this.options.hasRowTitle)
-                HTML += "<TH class = 'th corner'>" + /*this.options.thContent*/i + "</TH>";
-            for (var j = 0; j < this.options.nCols; j++)
-                HTML += "<TH class = 'th'>" + this.options.thContent + "</TH>";
+            if(this.settings.hasRowTitle)
+                HTML += "<TH class = 'th corner'>" + this.settings.thContent(i) + "</TH>";
+            for (var j = 0; j < this.settings.nCols; j++)
+                HTML += "<TH class = 'th'>" + this.settings.thContent(j) + "</TH>";
             HTML += "</TR>";
 
         }
 
         return HTML += "</THEAD>";
-    },
+};
     
-    _generateHTMLBody: function() {
+TableGenerator.prototype._generateHTMLBody = function() {
         var HTML = "<TBODY class = 'tbody'>";
 
-        for (var i = 0; i < this.options.nRowsB; i++) {
+        for (var i = 0; i < this.settings.nRowsB; i++) {
 
             HTML += "<TR class = 'trb'>"
-            if(this.options.hasRowTitle)
-                HTML += "<TH class = 'th'>" + /*this.options.thContent*/i + "</TH>";
-            for (var j = 0; j < this.options.nCols; j++) {
-                HTML += "<TD class = 'tdb'>" + this.options.tdContent + "</TD>";
+            if(this.settings.hasRowTitle)
+                HTML += "<TH class = 'th'>" + this.settings.thContent(i) + "</TH>";
+            for (var j = 0; j < this.settings.nCols; j++) {
+                HTML += "<TD class = 'tdb'>" + this.settings.tdContent(i, j) + "</TD>";
             }
             HTML += "</TR>";
 
         }
 
         return HTML += "</TBODY>";
-    },
+};
 
-    _generateHTMLFoot: function() {
+TableGenerator.prototype._generateHTMLFoot = function() {
         var HTML = "<TFOOT class = 'tfoot'>";
 
-        for (var i = 0; i < this.options.nRowsF; i++) {
+        for (var i = 0; i < this.settings.nRowsF; i++) {
 
             HTML += "<TR class = 'trf'>"
-            if(this.options.hasRowTitle)
-                HTML += "<TH class = 'th corner'>" + this.options.thContent + "</TH>";
-            for (var j = 0; j < this.options.nCols; j++) {
-                HTML += "<TD class = 'tdf'>" + this.options.tdContent + "</TD>";
+            if(this.settings.hasRowTitle)
+                HTML += "<TH class = 'th corner'>" + this.settings.settings(i) + "</TH>";
+            for (var j = 0; j < this.settings.nCols; j++) {
+                HTML += "<TD class = 'tdf'>" + this.settings.tdContent(i, j) + "</TD>";
             }
             HTML += "</TR>";
 
         }
 
         return HTML += "</TFOOT>";
-    }
-
 };
